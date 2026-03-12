afe17b8335f921911328166c3f5b6f5b87984cf3
Khushmeet <k6kaur@uwaterloo.ca>
2026-03-12 16:43:12 -0400

    gui prompts removed from green test

--- test/test_green_detection.py
+++ test/test_green_detection.py
@@ -5,25 +5,27 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from perception import OpenCVCamera, Perception
 
-
 cam = OpenCVCamera()
 p = Perception(cam)
 
-while True:
+try:
+    while True:
+        frame = cam.get_frame()
+        res = p.detect_green_box(frame)
 
-    frame = cam.get_frame()
+        if res.detected:
+            print(
+                f"Green box detected | area={res.area:.0f} error_x={res.error_x}"
+            )
 
-    res = p.detect_green_box(frame)
+        # Skip GUI if no monitor
+        # cv2.imshow("Green Box Test", frame)
+        # if cv2.waitKey(1) == ord("q"):
+        #     break
 
-    if res.detected:
-        print(
-            f"Green box detected | area={res.area:.0f} error_x={res.error_x}"
-        )
+except KeyboardInterrupt:
+    print("Test stopped by user")
 
-    # cv2.imshow("Green Box Test", frame)
-
-    # if cv2.waitKey(1) == ord("q"):
-    #     break
-
-cam.release()
-cv2.destroyAllWindows()
+finally:
+    cam.release()
+    cv2.destroyAllWindows()
