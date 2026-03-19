cdb1d9f51e1bbb73fa6fcb5136fdc185d60277a8
Khushmeet <k6kaur@uwaterloo.ca>
2026-03-18 22:27:16 -0400

    target specification test code, to be reviewed and tested

--- test/test_target_detection.py
+++ test/test_target_detection.py
@@ -6,19 +6,26 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from perception import OpenCVCamera, Perception
 from controllers import AlignmentController, ApproachController
+from controllers import LineFollowingController
 from drivebase import DriveBase
 import numpy as np
+
 
 
 def main():
 
     cam = OpenCVCamera()
-    p = Perception(cam,True)
-    lw_dected = False
+    p = Perception(cam,False)
+    lw_detected = False
     aligned = False
-    taget_aligner = AlignmentController()
+    target_aligner = AlignmentController()
     approach_targer = ApproachController()
     drivebase = DriveBase()
+    # From red_line_follow.py setting same controller values
+    line_follower = LineFollowingController(k_heading_slow=0, v_min=0.25, v_max=0.7, omega_max=0.15)
+    line_follower.lateral_pd.update_params(kp=0.2, kd=0.02)
+
+
 
     try:
         prev_t = time.monotonic()
@@ -30,16 +37,34 @@ def main():
 
             frame = cam.get_frame()
             
-            if lw_dected is not True:
+            if lw_detected is not True:
+
+
                 output = p.detect_target_cheap(frame)
                 if output.detected:
                     print(f"found w {output.bpx} px" f"output.detected={output.detected}")
-                    lw_dected = True
+                    lw_detected = True
+                    drivebase.stop()
                     cv2.destroyAllWindows()
+                    continue #skip rest of loop
                 else:
                     print(f"Not FOUND w {output.bpx} px" f"output.detected={output.detected}")
             
-            if (lw_dected):
+# khush updates, need review --> moving to a separate file
+            # if lw_detected is not True:
+
+            #     output = p.detect_target_cheap(frame)
+            #     if output.detected:
+            #         print(f"found w {output.bpx} px" f"output.detected={output.detected}")
+            #         lw_detected = True
+            #         drivebase.stop()
+            #         cv2.destroyAllWindows()
+            #         continue #skip rest of loop
+            #     else:
+            #         print(f"Not FOUND w {output.bpx} px" f"output.detected={output.detected}")
+# end of khush updates
+            
+            if (lw_detected):
                 
                 if not aligned:
                     
@@ -52,10 +77,10 @@ def main():
                             f"e_y = {np.round(result.error_y,2)} "
                             f"Blue area = {np.round(result.area,2)} "
                         )
-                        if abs(result.error_x) < taget_aligner.x_tol:
+                        if abs(result.error_x) < target_aligner.x_tol:
                             aligned = True
 
-                        command = taget_aligner.compute(result, dt)
+                        command = target_aligner.compute(result, dt)
                         print(command.v, command.omega)
                         drivebase.set_Velocity(command.v , command.omega)
                 else: 
